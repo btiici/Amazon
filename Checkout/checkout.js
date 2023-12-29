07b04fdbebd13d67c1ab09688be4e3c2c8c4c49f
btiici <olanrewajupelumi8@gmail.com>
2023-12-29 13:28:20 +0100

    deleted products
    delete products from cart

--- Checkout/checkout.js
+++ Checkout/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from "../Cart/cart.js"
+import {cart, removeFromCart} from "../Cart/cart.js"
 import { product } from "../data.js";
 
 let cartSummaryHtml ='';
@@ -35,7 +35,7 @@ cart.forEach((cartItem) => {
                        <span class="update-quantity-link link-primary">
                        Update
                        </span>
-                       <span class="delete-quantity-link link-primary">
+                       <span class="delete-quantity-link link-primary js-delete-link" data-product-id = "${matchingProduct.id}">
                        Delete
                        </span>
                </div>
@@ -47,7 +47,7 @@ cart.forEach((cartItem) => {
                <div class="delivery-option">
                    <input type="radio" checked
                        class="delivery-option-input"
-                       name="delivery-option-1">
+                       name="delivery-option-${matchingProduct.id}">
                        <div class="delivery-option-date">
                        Tuesday, June 21
                        </div>
@@ -58,7 +58,7 @@ cart.forEach((cartItem) => {
                <div class="delivery-option">
                <input type="radio"
                    class="delivery-option-input"
-                   name="delivery-option-1">
+                   name="delivery-option-${matchingProduct.id}">
                <div>
                    <div class="delivery-option-date">
                    Wednesday, June 15
@@ -71,7 +71,7 @@ cart.forEach((cartItem) => {
                <div class="delivery-option">
                <input type="radio"
                    class="delivery-option-input"
-                   name="delivery-option-1">
+                   name="delivery-option-${matchingProduct.id}">
                    <div>
                    <div class="delivery-option-date">
                        Monday, June 13
@@ -87,3 +87,12 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector('.js-orders').innerHTML = cartSummaryHtml
+
+document.querySelectorAll('.js-delete-link')
+ .forEach((link) => {
+    link.addEventListener('click', () => {
+        const productId = link.dataset.productId;
+        removeFromCart(productId);
+        console.log(cart);
+    });
+ });
