d5714ffcb76c60c62458b6849f90a6eb9bb2c9cf
btiici <olanrewajupelumi8@gmail.com>
2024-02-13 16:50:42 +0100

    create automated test for addtocart

--- Cart/cart.js
+++ Cart/cart.js
@@ -1,16 +1,23 @@
-export let cart = JSON.parse(localStorage.getItem('Cart'));
+export let cart;
 
-if (!cart) {
-    cart =[{
-        productId :"54e0eccd-8f36-462b-b68a-8182611d9add",
-        quantity: 2,
-        deliveryOptionsId: '1'
-    },
-    {
-        productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity : 1, 
-        deliveryOptionsId: '2'
-    }];
+loadFromStorage ();
+
+export function loadFromStorage  () {
+    cart = JSON.parse(localStorage.getItem('Cart'));
+
+    if (!cart) {
+        cart =[{
+            productId :"54e0eccd-8f36-462b-b68a-8182611d9add",
+            quantity: 2,
+            deliveryOptionsId: '1'
+        },
+        {
+            productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            quantity : 1, 
+            deliveryOptionsId: '2'
+        }];
+    }
+    
 }
 
 export function saveToLocalStorage () {
